cbebd187f66daf945beb70a0351c29126605db12
onyeka precious <onyekaprecious539@gmail.com>
2026-08-18 13:53:08 +0100

    nav init

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+export default function Navbar (){
+    const navItems: object[] = [
+        {
+            label:"Home",
+            url: "/" 
+        },
+        {
+            label:"About Us",
+            url: "/about" 
+        },
+        {
+            label:"Contact Us",
+            url: "/contact" 
+        },
+        {
+            label:"Explore Skills",
+            url: "/explore" 
+        }
+    ]
+    return(
+        <main className="flex items-center justify-between shadow-sm md:px-10 md:py-2 max-md:p-2">
+            <Link href={"/"} className="flex items-center gap-1">
+              <Image
+               src={"/logo.png"}
+               alt="logo"
+               width={400}
+               height={400}
+               className="w-10 h-10"
+              />   
+              <p className="text-xl font-light">SkillBridge</p>         
+            </Link>
+
+            <div className="flex items-center gap-5">
+                {
+                    navItems.map((item, index)=>(
+                        <Link key={index} href={"item.url"} className="text-xl hover:text-[#2E2910] transition-all duration-200">{item.label}</Link>
+                    ))
+                }
+                
+            </div>
+            <div className="flex items-center gap-5">
+                <Link href={"#"} className="text-xl border-2 px-2 rounded-sm ">Advertise Skill</Link>
+                <Link href={"#"} className="flex items-center text-xl gap-1 bg-[#2E2910] text-white px-3 rounded-sm border border-[#2E2910] py-1 group"><FaUserPlus 
+                className="group-hover " Register</Link>
+                
+            </div>
+        </main>
+    )
+}
