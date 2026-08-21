5b08002141e47a576d45f65aa52e5ead4f0fcd1e
onyeka precious <onyekaprecious539@gmail.com>
2026-08-21 16:56:27 +0100

    updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
+import { FaUserPlus } from "react-icons/fa";
+
 
 
 export default function Navbar (){
+    const [navOpen, setNavOpen]=useState(false)
+    // console.log(navOpen);
+    
     const navItems: object[] = [
         {
             label:"Home",
@@ -29,12 +35,12 @@ export default function Navbar (){
                alt="logo"
                width={400}
                height={400}
-               className="w-10 h-10"
+               className="w-10 h-10 flex"
               />   
               <p className="text-xl font-light">SkillBridge</p>         
             </Link>
 
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-5 max-lg:hidden">
                 {
                     navItems.map((item, index)=>(
                         <Link key={index} href={"item.url"} className="text-xl hover:text-[#2E2910] transition-all duration-200">{item.label}</Link>
@@ -42,7 +48,7 @@ export default function Navbar (){
                 }
                 
             </div>
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-5 max-lg:">
                 <Link href={"#"} className="text-xl border-2 px-2 rounded-sm ">Advertise Skill</Link>
                 <Link href={"#"} className="flex items-center text-xl gap-1 bg-[#2E2910] text-white px-3 rounded-sm border border-[#2E2910] py-1 group"><FaUserPlus 
                 className="group-hover " Register</Link>
