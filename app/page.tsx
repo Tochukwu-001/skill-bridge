e2d74dd4a51dba2ade9a866f4fcf64137855c17a
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-08-21 17:59:08 +0100

    sam

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-import { CiUser } from "react-icons/ci";
-import Button from '@mui/material/Button';
+import { Theme } from "@/components/Theme";
+import Link from "next/link";
+
 
 
 export default function Home() {
   return (
     <main>
-      Home Page
-      <CiUser className="text-7xl text-purple-700" />
-      <Button variant="contained">Hello world</Button>
+      <section className="min-h-dvh bg-[url('/image.png')] bg-no-repeat bg-center bg-cover">
+        <div className="min-h-dvh bg-black/60">
+          <div className="w-2/3 mx-auto space-y-10 pt-20">
+            <h2 className="text-white uppercase text-4xl font-bold text-center">practice . learn . achieve</h2>
+            <p className="text-2xl font-thin text-white text-center">Bridging the gap between what you know, can do, and the opportunities waiting for you. Turning potentials into progress. Connect with the right people and take the next step towards your future.</p>
+            <div className="flex items-center gap-10 justify-center">
+              <Link href={"#"} className="text-white text-xl px-10 py-4 rounded-full" style={{backgroundColor: Theme.darkGreen}}>Start Now</Link>
+              <Link className="border text-white px-10 py-4 rounded-full text-xl" href={"#"}>Learn More</Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
