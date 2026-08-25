d1c478207765890c0bbffce5a58a511cc974431f
onyeka precious <onyekaprecious539@gmail.com>
2026-08-25 12:24:42 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import { CiUser } from "react-icons/ci";
-import Button from '@mui/material/Button';
+
 
 
 export default function Home (){
     return(
         <main>
-            Home Page
-            <CiUser className="text-7xl text-purple-700" />
-            <Button variant="contained">Hello world</Button>
+            <section className="min-h-dvh bg-[url('/image.png')] bg-no-repeat bg-center bg-cover">
+                <div className="min-h-dvh bg-black/60">
+                    <div className="w-2/3 mx-auto space-y-10 pt-20">
+                        <h2 className="text-white uppercase text-4xl font-bold text-center">practice . learn . achieve</h2>
+                        <p className="text-2xl font-thin text-2xl text-center">Bridging the gap between what you know, can do, and the opportunities waiting for you. Turning potentials into progress. Connect with the right people and take the next step towards your future</p>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
